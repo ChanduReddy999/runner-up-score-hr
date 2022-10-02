f8b60a38515250ab13a8b9d5617e24000356465a
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-10-02 23:10:45 +0530

    Add files via upload

---
+++ runner_up_score_hr.py
@@ -0,0 +1,17 @@
+"""Runner up score in python hr"""
+
+"""method 1"""
+
+# n=int(input())
+# arr=map(int, input().split())
+# arr=list(set(list(arr)))
+# ar=len(arr)
+# arr=sorted(arr)
+# print(arr[ar-2])
+
+
+"""method 2"""
+
+# n=int(input())
+# arr=map(int,input().split())
+# print(sorted(set(arr))[-2])
